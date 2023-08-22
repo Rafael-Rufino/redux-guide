e1fb818fbaacd816d7d04933708f4f1737d9f7e2
RafaelRufino <rafaelrufino.52@gmail.com>
2023-08-22 19:58:13 -0300

    feat: create header component

---
+++ src/components/header/index.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+import Cart from "../cart/index";
+
+import * as S from "./styles";
+import { BsCartPlusFill } from "react-icons/bs";
+import { FiLogOut } from "react-icons/fi";
+function Header() {
+  const [cartIsVisible, setCartIsVisible] = useState(false);
+
+  const handleCartClick = () => {};
+
+  const handleLoginClick = () => {};
+
+  const handleLogoutClick = () => {};
+  const currentUser = true;
+  return (
+    <S.Container>
+      <S.Logo>Lojinha</S.Logo>
+      <S.Buttons>
+        {currentUser ? (
+          <div onClick={handleLogoutClick}>
+            {" "}
+            <FiLogOut /> Sair
+          </div>
+        ) : (
+          <div onClick={handleLoginClick}>Login</div>
+        )}
+
+        <div onClick={handleCartClick}>
+          {" "}
+          <BsCartPlusFill /> Carrinho [{1}]
+        </div>
+      </S.Buttons>
+
+      <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
+    </S.Container>
+  );
+}
+
+export default Header;
