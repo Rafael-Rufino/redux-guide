4264a9ed72bbf9b25ae4ac59042724ddce3e3da5
RafaelRufino <rafaelrufino.52@gmail.com>
2023-08-22 19:58:03 -0300

    feat: create footer component

---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import * as S from "./styles";
+
+export function Footer() {
+  return (
+    <S.Container>
+      <S.Text>© 2023 - All rights reserved</S.Text>
+    </S.Container>
+  );
+}
---
+++ src/components/footer/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+
+  width: 100%;
+  height: 80px;
+  background: black;
+`;
+
+export const Text = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
