3e61dc491fc035ff25a1f0af3e17c7e71db6a363
RafaelRufino <rafaelrufino.52@gmail.com>
2023-08-22 19:57:22 -0300

    feat: create cart component

---
+++ src/components/cart/index.jsx
@@ -0,0 +1,16 @@
+import * as S from "./styles";
+
+const Cart = ({ isVisible, setIsVisible }) => {
+  const handleEscapeAreaClick = () => setIsVisible(false);
+
+  return (
+    <S.CartContainer isVisible={isVisible}>
+      <S.CartEscapeArea onClick={handleEscapeAreaClick} />
+      <S.CartContent>
+        <S.CartTitle>Seu Carrinho</S.CartTitle>
+      </S.CartContent>
+    </S.CartContainer>
+  );
+};
+
+export default Cart;
