911617b87b7ed6c325aee4ddafc4d0124f0ced63
RafaelRufino <rafaelrufino.52@gmail.com>
2023-08-23 16:04:29 -0300

    feat: create cartReducer

---
+++ src/redux/cart/reducer.js
@@ -0,0 +1,64 @@
+import CartActionTypes from "./action-types";
+
+const initialState = {
+  products: [],
+};
+const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CartActionTypes.ADD_PRODUCT_T0_CART:
+      const productIsAlreadyInCart = state.products.some(
+        (product) => product.id === action.payload.id
+      );
+
+      return {
+        ...state,
+        products: productIsAlreadyInCart
+          ? state.products.map((product) =>
+              product.id === action.payload.id
+                ? { ...product, quantity: product.quantity + 1 }
+                : product
+            )
+          : [
+              ...state.products,
+              {
+                ...action.payload,
+                quantity: 1,
+              },
+            ],
+      };
+
+    case CartActionTypes.INCREASE_PRODUCT_QUANTITY:
+      return {
+        ...state,
+        products: state.products.map((product) =>
+          product.id === action.payload
+            ? { ...product, quantity: product.quantity + 1 }
+            : product
+        ),
+      };
+
+    case CartActionTypes.DECREASE_PRODUCT_QUANTITY:
+      return {
+        ...state,
+        products: state.products
+          .map((product) =>
+            product.id === action.payload
+              ? { ...product, quantity: product.quantity - 1 }
+              : product
+          )
+          .filter((product) => product.quantity > 0),
+      };
+
+    case CartActionTypes.REMOVE_PRODUCT:
+      return {
+        ...state,
+        products: state.products.filter(
+          (product) => product.id !== action.payload
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
+export default cartReducer;
