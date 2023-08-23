db33aca858211cbd72a90cf564ebe9dae09ad840
RafaelRufino <rafaelrufino.52@gmail.com>
2023-08-23 16:01:45 -0300

    feat: add cart product

--- src/components/product-item/index.jsx
+++ src/components/product-item/index.jsx
@@ -1,14 +1,24 @@
 import { BsCartPlus } from "react-icons/bs";
+import { useDispatch } from "react-redux";
 
 import CustomButton from "../custom-button/index";
 
 import * as S from "./styles";
+import { addProductToCart } from "../../redux/cart/actions";
 
 const ProductItem = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const handleProductToCartClick = () => {
+    dispatch(addProductToCart(product));
+  };
   return (
     <S.ProductContainer>
       <S.ProductImage imageUrl={product.imageUrl}>
-        <CustomButton startIcon={<BsCartPlus />}>
+        <CustomButton
+          startIcon={<BsCartPlus />}
+          onClick={handleProductToCartClick}
+        >
           Adicionar ao carrinho
         </CustomButton>
       </S.ProductImage>
