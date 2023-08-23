f81c8f5db5a65ef1531fb6e5c5949081389bcbdf
RafaelRufino <rafaelrufino.52@gmail.com>
2023-08-23 16:03:20 -0300

    feat: create cartActionTypes

---
+++ src/redux/cart/action-types.js
@@ -0,0 +1,8 @@
+const CartActionTypes = {
+  ADD_PRODUCT_T0_CART: "cart/addProductToCart",
+  INCREASE_PRODUCT_QUANTITY: "cart/increaseProductQuantity",
+  DECREASE_PRODUCT_QUANTITY: "cart/decreaseProductQuantity",
+  REMOVE_PRODUCT: "cart/removeProduct",
+};
+
+export default CartActionTypes;
