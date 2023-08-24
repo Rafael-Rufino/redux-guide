1a82fa191db5a05b4598a40df089c4d894ff5d2d
RafaelRufino <rafaelrufino.52@gmail.com>
2023-08-24 19:03:05 -0300

    chore: change logic reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/reducer";
-import cartReducer from "./cart/reducer";
+import cartReducer from "./cart/slice";
+//import cartReducer from "./cart/reduce";
 
 const rootReducer = combineReducers({
   userReducer,
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,21 @@
-import { createStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+
+import rootReducer from "./root-reducer";
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [logger],
+});
+
+export default store;
+
+// Primero metado sem usar a ferramenta  @reduxjs/toolkit
+/*import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
 const store = createStore(rootReducer, applyMiddleware(logger));
 
-export default store;
+export default store;*/
